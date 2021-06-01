f040f3bf64160d83c187f6647eb49e67d459f6ae
Shivani Modi <shivani.modi@live.com>
2021-06-01 12:21:53 -0700

    updated sizeMenu so you can change size of just highlighted text, instead of changing the entire pane

--- src/sizeMenu.java
+++ src/sizeMenu.java
@@ -1,11 +1,8 @@
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.event.*;
+import javax.swing.text.*;
 
-public class sizeMenu extends JMenu implements ActionListener {
+public class sizeMenu extends JMenu{
     JFrame frame;
     JTextPane pane;
     JSpinner fontSizeSpinner;
@@ -16,18 +13,34 @@ public class sizeMenu extends JMenu implements ActionListener {
         pane = p;
 
         fontSizeSpinner = new JSpinner();
-        fontSizeSpinner.setValue(20);
+        fontSizeSpinner.setValue(pane.getFont().getSize());
+
         this.add(fontSizeSpinner);
         fontSizeSpinner.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
-                pane.setFont(new Font(pane.getFont().getFamily(), Font.PLAIN, (int) fontSizeSpinner.getValue()));
+                //pane.setFont(new Font(pane.getFont().getFamily(), Font.PLAIN, (int) fontSizeSpinner.getValue()));
+
+                StyledDocument doc = pane.getStyledDocument();
+                Style style = pane.addStyle("", null);
+
+                int start = pane.getSelectionStart();
+                int end = pane.getSelectionEnd();
+                //no selection
+                if (start == end) {
+                    return;
+                }
+                //backwards selection (select starting from back end)
+                if (start > end) { // Backwards selection?
+                    int temp = start;
+                    start = end;
+                    end = temp;
+                }
+                StyleConstants.setFontSize(style, (int)fontSizeSpinner.getValue());
+                doc.setCharacterAttributes(start, end - start, style, false);
             }
         });
     }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
 }
+
+
